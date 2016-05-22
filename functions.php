82d8a3cae897b0682962aac01af3b25a8c02c182
ginasuci <genbon52@gmail.com>
2016-05-23 02:42:00 +0700

    rewrite style

--- functions.php
+++ functions.php
@@ -5,11 +5,9 @@ function powen_lite_child_enqueue() {
     wp_enqueue_style( 'powen-lite-style', get_template_directory_uri() . '/style.css');
 }
 
-add_filter( 'woothemes_our_team_item_template', 'new_team_member_template' );
-
-function new_team_member_template( $tpl ) {
-    $tpl = '<div itemscope itemtype="http://schema.org/Person" class="%%CLASS%%">%%TITLE%% %%AVATAR%% <div id="team-member-%%ID%%" class="team-member-text" itemprop="description">%%TEXT%% %%AUTHOR%%</div></div>';
-    return $tpl;
-}
-
+////enqueue our locally supplied font awesome stylesheet
+//function enqueue_our_required_stylesheets() {
+//    wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css');
+//}
+//add action( 'wp_enqueue_scripts', 'enqueue_our_required_stylesheets');
 ?>
