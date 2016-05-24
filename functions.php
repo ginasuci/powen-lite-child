2be71a9a9ae67f17ba6a5e77693e60107faaf12c
ginasuci <genbon52@gmail.com>
2016-05-24 15:59:34 +0700

    contact page

--- functions.php
+++ functions.php
@@ -5,9 +5,10 @@ function powen_lite_child_enqueue() {
     wp_enqueue_style( 'powen-lite-style', get_template_directory_uri() . '/style.css');
 }
 
-////enqueue our locally supplied font awesome stylesheet
-//function enqueue_our_required_stylesheets() {
-//    wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css');
-//}
-//add action( 'wp_enqueue_scripts', 'enqueue_our_required_stylesheets');
+function load_fonts() {
+            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Roboto');
+            wp_enqueue_style( 'googleFonts');
+        }
+    
+    add_action('wp_print_styles', 'load_fonts');
 ?>
