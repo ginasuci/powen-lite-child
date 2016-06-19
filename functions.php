48602c5765b53f8c1a876ff2cc1d3e94b626fdf4
ginasuci <genbon52@gmail.com>
2016-06-19 08:49:32 +0700

    #16

--- functions.php
+++ functions.php
@@ -6,15 +6,15 @@ function powen_lite_child_enqueue() {
 }
 
 function load_google_fonts() {
-            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Roboto');
+            wp_register_style('googleFontsRoboto', 'http://fonts.googleapis.com/css?family=Roboto');
             wp_enqueue_style('googleFontsRoboto');
     
-            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Righteous');
+            wp_register_style('googleFontsRighteous', 'https://fonts.googleapis.com/css?family=Righteous');
             wp_enqueue_style('googleFontsRighteous');
     
-            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Cairo');
+            wp_register_style('googleFontsCairo', 'https://fonts.googleapis.com/css?family=Cairo');
             wp_enqueue_style('googleFontsCairo');
         }
     
     add_action('wp_print_styles', 'load_google_fonts');
-?>
+?> 
